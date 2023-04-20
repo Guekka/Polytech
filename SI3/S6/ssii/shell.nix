2bd4a94f47c3f14121196e1e8d811f2c0ca5c49a
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-04-20 18:06:39 +0200

    ssii: things

--- SI3/S6/ssii/shell.nix
+++ SI3/S6/ssii/shell.nix
@@ -1,16 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs; # Same for pkgs.
-
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c17904b9802a35acf088ee315b669bd4be8f7633.tar.gz") {}
+}:
+with pkgs;
   mkShell {
     buildInputs = [
-
       # Defines a python + set of packages.
       (python3.withPackages (ps:
         with ps;
         with python3Packages; [
+          vscodium
           jupyter
           ipython
+          ipykernel
+          isort
+          notebook
+          wget
 
           numpy
           matplotlib

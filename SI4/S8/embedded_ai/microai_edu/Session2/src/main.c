3f2e2e0fb62231851d15d0963001e3b4125a9d1f
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-02-15 20:30:01 +0100

    embedded td2 first working version

--- SI4/S8/embedded_ai/microai_edu/Session2/src/main.c
+++ SI4/S8/embedded_ai/microai_edu/Session2/src/main.c
@@ -24,6 +24,12 @@ static const struct gpio_dt_spec leds[LEDS_COUNT] = {
 static const struct gpio_dt_spec keys[KEYS_COUNT] = {
     DT_FOREACH_CHILD_SEP(DT_PATH(gpio_keys), GPIO_DT_SPEC_GET_GPIOS, (, ))};
 
+// unfortunately, switches and buttons are on the same port
+#define SWITCHES_START 0
+#define SWITCHES_END 8
+#define BUTTONS_START 8
+#define BUTTONS_END 16
+
 #define SEVENSEGMENT0_PINS_COUNT (DT_FOREACH_PROP_ELEM(DT_NODELABEL(sevensegment0), gpios, PROP_COUNT) 0)
 static const struct gpio_dt_spec sevensegment0[SEVENSEGMENT0_PINS_COUNT] = {
     DT_FOREACH_PROP_ELEM_SEP(DT_NODELABEL(sevensegment0), gpios, GPIO_DT_SPEC_GET_BY_IDX, (, )),
@@ -63,6 +69,10 @@ void display_show_value(uint16_t i)
     gpio_port_set_masked(port_7seg23, 0xFF00, sevensegLUT[(i / 1000) % 10] << 8);
 }
 
+static int ticks_when_test_started = 0;
+static int ticks_when_user_pressed = 0;
+static int results[10] = {0};
+
 void store_result(int *results, int result, int size)
 {
     for (int i = 0; i < size; i++)
@@ -88,12 +98,12 @@ void display_average(int *results, int size)
     }
 
     const int average = sum / count;
-    display_value(average);
+    display_show_value(average);
 }
 
 void store_button_press_time(int *store_to)
 {
-    const int time = HAL_GetTick();
+    const int time = k_uptime_get();
     const int BOUNCE_DELAY = 300;
     const int delta = time - *store_to;
     if (delta < BOUNCE_DELAY)
@@ -104,48 +114,40 @@ void store_button_press_time(int *store_to)
 
 int number_of_toggled_switches()
 {
-    const int switches[] = {SW0_Pin, SW1_Pin, SW2_Pin, SW3_Pin, SW4_Pin, SW5_Pin, SW6_Pin, SW7_Pin};
     int count = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        if (HAL_GPIO_ReadPin(GPIOC, switches[i]) == GPIO_PIN_SET)
-            ++count;
-    }
+    for (size_t i = SWITCHES_START; i < SWITCHES_END; ++i)
+        count += gpio_pin_get(port_swbtn, i);
     return count;
 }
-
-static int ticks_when_test_started = 0;
-static int ticks_when_user_pressed = 0;
-static int results[10] = {0};
 
 bool is_signal_time(int number_of_switches)
 {
     const bool random_validated = rand() < RAND_MAX / 100000;
     const int minimum = 3000 + number_of_switches * 1000;
 
-    const int elapsed = HAL_GetTick() - ticks_when_test_started;
+    const int elapsed = k_uptime_get() - ticks_when_test_started;
 
     return random_validated && elapsed > minimum;
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+static struct gpio_callback button_pressed_cb_data;
+static void button_pressed(const struct device *dev, struct gpio_callback *cb, unsigned int pins)
 {
-    switch (GPIO_Pin)
+    if (pins & BIT(BUTTONS_START + 1))
     {
-    case BTN1_Pin:
         store_button_press_time(&ticks_when_test_started);
-        srand(HAL_GetTick());
-        log("Test started. Ticks: %i", ticks_when_test_started);
-
-        break;
-    case BTN2_Pin:
-        log("Displaying average");
+        printk("Test started. Ticks: %i\n", ticks_when_test_started);
+        srand(k_uptime_get());
+    }
+    if (pins & BIT(BUTTONS_START + 2))
+    {
+        printk("Displaying average\n");
         display_average(results, 10);
-        break;
-    case BTN4_Pin:
+    }
+    if (pins & BIT(BUTTONS_START + 4))
+    {
         store_button_press_time(&ticks_when_user_pressed);
-        log("User pressed the button. Ticks: %i", ticks_when_user_pressed);
-        break;
+        printk("User pressed the button. Ticks: %i", ticks_when_user_pressed);
     }
 }
 
@@ -153,7 +155,7 @@ int init(void)
 {
     int ret = 0;
 
-    if (!device_is_ready(port_io) || !device_is_ready(port_led) || !device_is_ready(port_swbtn) || !device_is_ready(port_7seg01) || !device_is_ready(port_7seg23) || !adc_is_ready_dt(&light0) || !device_is_ready(accel0))
+    if (!device_is_ready(port_io) || !device_is_ready(port_led) || !device_is_ready(port_swbtn) || !device_is_ready(port_7seg01) || !device_is_ready(port_7seg23))
     {
         return 1;
     }
@@ -165,6 +167,7 @@ int init(void)
     for (size_t i = 0; i < KEYS_COUNT; i++)
     {
         ret += gpio_pin_configure_dt(keys + i, GPIO_INPUT);
+        ret += gpio_pin_interrupt_configure_dt(keys + i, GPIO_INT_EDGE_TO_ACTIVE);
     }
     for (size_t i = 0; i < SEVENSEGMENT0_PINS_COUNT; i++)
     {
@@ -183,31 +186,12 @@ int init(void)
         ret += gpio_pin_configure_dt(sevensegment3 + i, GPIO_OUTPUT_ACTIVE);
     }
 
-    ret += adc_channel_setup_dt(&light0);
-
-    /* Light sensor init*/
-    // ADC buffer
-    uint16_t light0_buf;
-    struct adc_sequence light0_sequence = {
-        .buffer = &light0_buf,
-        .buffer_size = sizeof(light0_buf), // buffer size in bytes, not number of samples
-    };
-
-    adc_sequence_init_dt(&light0, &light0_sequence);
-
-    /* Accelerometer init*/
-    struct sensor_value accel0_config;
-    /* set accel/gyro sampling frequency to 104 Hz */
-    accel0_config.val1 = 26;
-    accel0_config.val2 = 0;
-    if (sensor_attr_set(accel0, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &accel0_config))
-    {
-        printk("Cannot set sampling frequency for accelerometer.\n");
-    }
-    if (sensor_attr_set(accel0, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &accel0_config))
-    {
-        printk("Cannot set sampling frequency for gyro.\n");
-    }
+    // interrupt
+    gpio_init_callback(&button_pressed_cb_data, button_pressed,
+                       BIT(BUTTONS_START + 1) |
+                           BIT(BUTTONS_START + 2) |
+                           BIT(BUTTONS_START + 4));
+    gpio_add_callback(port_swbtn, &button_pressed_cb_data);
 
     return ret;
 }
@@ -220,13 +204,63 @@ int main(void)
     }
 
     display_show_value(0);
+    printk("Starting...\n");
 
-    k_thread_start(task1_thread_id);
-    k_thread_start(task2_thread_id);
+    int ticks_when_signal_fired = 0;
+    printk("%i toggled switches at start\n", number_of_toggled_switches());
 
     while (1)
     {
-        k_msleep(SLEEP_TIME_MS);
+        // for some reason,
+        k_msleep(1);
+
+        if (ticks_when_test_started == 0)
+            continue;
+
+        // test has started.
+
+        // and the signal has not been fired yet
+        if (ticks_when_signal_fired == 0)
+        {
+            // Make the LEDs blink. Half a second even, half a second odd.
+            const int time = k_uptime_get() - ticks_when_test_started;
+            const int period = 1000;
+
+            // turn on even LEDs for half a second, then odd LEDs for half a second
+            const int is_even = (time / period) % 2 == 0;
+            for (size_t i = 0; i < LEDS_COUNT; i++)
+            {
+                const int is_on = (i % 2 == 0) == is_even;
+                gpio_pin_set_dt(leds + i, is_on);
+            }
+
+            if (is_signal_time(number_of_toggled_switches()))
+            {
+                ticks_when_signal_fired = k_uptime_get();
+                printk("Firing signal at ticks: %i\n", ticks_when_signal_fired);
+                // light up all LEDs
+                for (size_t i = 0; i < LEDS_COUNT; i++)
+                    gpio_pin_set(port_led, leds[i].pin, 1);
+            }
+        }
+
+        if (ticks_when_user_pressed == 0)
+            continue;
+
+        if (ticks_when_signal_fired == 0)
+            // the user pressed the button too early
+            continue;
+
+        // test has started and the button has been pressed. Display the time.
+        const int reaction_time = ticks_when_user_pressed - ticks_when_signal_fired;
+        printk("Reaction time: %i\n", reaction_time);
+
+        store_result(results, reaction_time, 10);
+
+        display_show_value(reaction_time);
+        ticks_when_signal_fired = 0;
+        ticks_when_user_pressed = 0;
+        ticks_when_test_started = 0;
     }
 
     return -1; // Should never reach this

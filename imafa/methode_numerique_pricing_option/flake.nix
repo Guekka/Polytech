b996bc9c5e6b7a1d038d114042fe063b8dd1677b
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-10-21 20:10:29 +0200

    pricing option things

---
+++ imafa/methode_numerique_pricing_option/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        # Defines a python + set of packages.
+        (pkgs.python3.withPackages (ps:
+          with ps;
+          with pkgs.python3Packages; [
+            jupyter
+            ipython
+            ipykernel
+            isort
+            notebook
+            wget
+            scipy
+
+            numpy
+            matplotlib
+            yapf
+          ]))
+      ];
+    };
+  };
+}
